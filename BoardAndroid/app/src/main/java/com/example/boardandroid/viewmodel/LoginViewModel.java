afc102866ffb338d0468869e5d2bc7a0e85110b8
CHAEWON NEW DESKTOP <kch9870@naver.com>
2022-07-07 00:12:38 +0900

    ADD 1. MultableLiveData 진행중

--- BoardAndroid/app/src/main/java/com/example/boardandroid/viewmodel/LoginViewModel.java
+++ BoardAndroid/app/src/main/java/com/example/boardandroid/viewmodel/LoginViewModel.java
@@ -1,8 +1,9 @@
 package com.example.boardandroid.viewmodel;
 
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 public class LoginViewModel extends ViewModel {
 
-
+    private MutableLiveData<> =
 }
